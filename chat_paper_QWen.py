5dbdc28777064bce599ada2c56cb576359a64a6f
Henry <zh02henry@gmail.com>
2024-12-17 12:54:30 +0800

    initialize

---
+++ chat_paper_QWen.py
@@ -0,0 +1,36 @@
+
+
+# 定义Reader类
+class Reader:
+    # 初始化方法，设置属性
+    def __init__(self, key_word, query, filter_keys,
+                 root_path='./',
+                 gitee_key='',
+                 sort=arxiv.SortCriterion.SubmittedDate, user_name='defualt', args=None):
+        self.user_name = user_name  # 读者姓名
+        self.key_word = key_word  # 读者感兴趣的关键词
+        self.query = query  # 读者输入的搜索查询
+        self.sort = sort  # 读者选择的排序方式
+        if args.language == 'en':
+            self.language = 'English'
+        elif args.language == 'zh':
+            self.language = 'Chinese'
+        else:
+            self.language = 'Chinese'
+        self.filter_keys = filter_keys  # 用于在摘要中筛选的关键词
+        self.root_path = root_path
+        # 创建一个ConfigParser对象
+        self.config = configparser.ConfigParser()
+        # 读取配置文件
+        self.config.read('apikey.ini')
+        # 获取某个键对应的值
+        self.chat_api_list = self.config.get('OpenAI', 'OPENAI_API_KEYS')[1:-1].replace('\'', '').split(',')
+        self.chat_api_list = [api.strip() for api in self.chat_api_list if len(api) > 5]
+        self.cur_api = 0
+        self.file_format = args.file_format
+        if args.save_image:
+            self.gitee_key = self.config.get('Gitee', 'api')
+        else:
+            self.gitee_key = ''
+        self.max_token_num = 4096
+        self.encoding = tiktoken.get_encoding("gpt2")
